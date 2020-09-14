6570f31b6dc78092789dd52eed1d3fa4f1ff598e
gerardo <zamora.gzm@gmail.com>
2020-09-14 15:42:49 -0700

    New Branch Feature-SortDesc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+    public void sortDesc(int[] intArray){
+        for(int i = 0; i < intArray.length-1; i++) {
+            for (int j = i+1; j< intArray.length; j++) {
+                if(intArray[i] < intArray[j]) {
+                    int temp = intArray[i];
+                    intArray[i] = intArray[j];
+                    intArray[j] = temp;
+                }
+            }
+        }
+    }
 }
---
+++ tests/sortDescTest.java
@@ -0,0 +1,24 @@
+import com.company.Main;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class sortDescTest {
+    int[] testArray;
+    int[] correctArray;
+    Main m;
+
+    @BeforeEach
+    void SetUp(){
+        testArray = new int[]{2,3,1,6,5,20,7,8,9,11};
+        correctArray = new int[]{20,11,9,8,7,6,5,3,2,1};
+        m = new Main();
+    }
+
+    @Test
+    void testWorking(){
+        m.sortDesc(testArray);
+        assertArrayEquals(correctArray,testArray);
+    }
+}
